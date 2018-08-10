122a4ee5ee72d26cbdccc3cc29f3e4ca431992e8
kevinjc <kevinjc@uio.no>
2018-08-11 00:45:10 +0200

    indecision app logic commited

--- src/app.js
+++ src/app.js
@@ -4,56 +4,98 @@ console.log('App.js is running');
 
 const template = <p> This is JSX from app.js! </p>;
 
+const app = {
+    title: 'indecision App',
+    subtitle: 'let the computer decide for you',
+    options: []
+};
 
-const user = {
-    name: 'Bob',
-    age: 26,
-    location: 'thisLocation'
+// e => event
+// remember to rerender
+const onFormSubmit = (e) => {
+    e.preventDefault();
+    const option = e.target.elements.option.value;
+
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        render();
+    }
+};
+
+const removeAll = () => {
+    app.options = [];
+    render();
+};
+
+const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.options.length);
+};
+
+const render = () => {
+    const templateTwo = (
+        <div>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+            <p>{app.options.length}</p>
+            <button disabled={app.options.length === 0} onClick={onMakeDecision}>Making the decision</button>
+
+            <button onClick={removeAll}>removeAll</button>
+            <ol>
+            {
+                app.options.map((option) => {
+                    return <li key={option}>{option}</li>
+              })
+            }
+                <li>item one</li>
+                <li>item two</li>
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option"/>
+                <button>Add Option</button>
+            </form>
+
+        </div>
+    );
+
+    ReactDOM.render(templateTwo, appRoot);
 }
 
-const templateTwo = (
-    <div>
-        <h1>apptitle</h1>
-        <p>app Subtitle</p>
-        <p>{user.name}</p>
-        <p>{user.age}</p>
-        <p>{user.location}</p>
-    </div>
-);
-
-
+const appRoot = document.getElementById('app');
+render();
 //className,
 // JSX has no databinding, need to rerender upon change
 
-let count = 0;
+// Addition challenge
+// let count = 0;
 
+// const addOne = () => {
+//     count++;
+//     renderCounterApp();
+// };
+// const minusOne = () => {
+//     count--;
+//     renderCounterApp();
+// };
 
-const addOne = () => {
-    count++;
-    renderCounterApp();
-};
-const minusOne = () => {
-    count--;
-    renderCounterApp();
-};
+// const reset = () => {
+//     count = 0;
+//     renderCounterApp();
+// };
 
-const reset = () => {
-    count = 0;
-    renderCounterApp();
-};
+// const appRoot = document.getElementById('app');
 
-const appRoot = document.getElementById('app');
+// const renderCounterApp = () => {
+//     const templateThree = (
+//         <div>
+//             <h1>Count: {count}</h1>
+//             <button onClick={addOne}>+1 </button>
+//             <button onClick={minusOne}>-1 </button>
+//             <button onClick={reset}>0 </button>
+//         </div>
+//     );
+//     ReactDOM.render(templateThree, appRoot);
+// }
 
-const renderCounterApp = () => {
-    const templateThree = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1 </button>
-            <button onClick={minusOne}>-1 </button>
-            <button onClick={reset}>0 </button>
-        </div>
-    );
-    ReactDOM.render(templateThree, appRoot);
-}
-
-renderCounterApp();
+// renderCounterApp();
