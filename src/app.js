1d78b8f1c2e384c802e5d6c96320d2caebe0a26f
kevinjc <kevinjc@uio.no>
2018-08-15 01:37:29 +0200

    converting to class property syntax

--- src/app.js
+++ src/app.js
@@ -5,3 +5,30 @@ import IndecisionApp from './components/IndecisionApp'
 
 const appRoot = document.getElementById('app');
 ReactDOM.render(<IndecisionApp options={['Devils den', 'district']}/>, appRoot);
+
+
+class OldSyntax {
+    constructor() {
+        this.name = 'mike';
+    }
+}
+const oldSyntax = new OldSyntax();
+console.log(oldSyntax);
+
+class Bork {
+    //Property initializer syntax
+    instanceProperty = "bork";
+    boundFunction = () => {
+      return this.instanceProperty;
+    }
+
+    //Static class properties
+    static staticProperty = "babelIsCool";
+    static staticFunction = function() {
+      return Bork.staticProperty;
+    }
+  }
+
+  let myBork = new Bork;
+  //Property initializers are not on the prototype.
+  console.log(myBork); // > undefined
